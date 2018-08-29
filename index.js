85a1a12c12d9b3dbbdf4bc66b85469659585897a
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-29 11:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  var array = [];
+  
+  for (var i=0; i < musicianArray.length; i++) {
+    var string = musicianArray[i] + ' plays ' + instrumentArray[i];
+    array.push(string);
+  }
+  
+  return array;
+}

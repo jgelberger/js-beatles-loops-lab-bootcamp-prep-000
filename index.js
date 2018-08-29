143209900e4f7cc484b12bdf72086fee1f230d69
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-29 12:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
 }
 
 function johnLennonFacts(factsArray) {
-    
+  var i = 0;
+  while (i < factsArray.length) {
+    factsArray[i] = factsArray[i] + '!!!';
+    i++;
+  }
+  return factsArray;
 }
